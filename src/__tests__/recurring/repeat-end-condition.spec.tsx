82cb82547e3a8926946635acafa270283f087996
이정민 <ljm5138@douzone.com>
2025-10-31 09:44:56 +0900

    test: 반복 종료 조건 지정 -> 반복 일정 생성 시 종료 날짜를 지정할 수 있다 - 테스트 설계

---
+++ src/__tests__/recurring/repeat-end-condition.spec.tsx
@@ -0,0 +1,32 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import { SnackbarProvider } from 'notistack';
+import { ReactElement } from 'react';
+
+import App from '../../App';
+
+const theme = createTheme();
+
+const setup = (element: ReactElement) => {
+  const user = userEvent.setup();
+
+  return {
+    ...render(
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SnackbarProvider>{element}</SnackbarProvider>
+      </ThemeProvider>
+    ),
+    user,
+  };
+};
+
+describe('반복 일정 기능 - 반복 종료 조건', () => {
+  describe('반복 종료 조건 지정', () => {
+    it('반복 일정 생성 시 종료 날짜를 지정할 수 있다', async () => {
+    
+    });
+  });
+});
